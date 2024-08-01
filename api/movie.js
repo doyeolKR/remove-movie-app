6f70dee610a6e0814465c530e9f42c2b671801ba
doyeol <oktui2002@naver.com>
2024-08-02 05:02:03 +0900

    update serverless function

--- api/movie.js
+++ api/movie.js
@@ -8,6 +8,6 @@ export default async function handler(request, response) {
     ? `https://omdbapi.com?apikey=${APIKEY}&i=${id}&plot=full`
     : `https://omdbapi.com?apikey=${APIKEY}&s=${title}&page=${page}`;
   const res = await fetch(url);
-  const json = res.json();
+  const json = await res.json();
   response.status(200).json(json);
 }
